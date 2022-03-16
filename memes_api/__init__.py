71b8d62715694d12b00e6489dcf422cfc61e8218
James Hodgkinson <james@terminaloutcomes.com>
2022-03-16 21:43:53 +1000

    closes #24 - moves to aioboto3

--- memes_api/__init__.py
+++ memes_api/__init__.py
@@ -2,16 +2,17 @@
 
 from functools import lru_cache
 from hashlib import sha1
+from importlib.util import module_from_spec
 from io import BytesIO
 import json
 import os.path
 from pathlib import Path
-from typing import Any, Dict, Generator, List, Optional, Union
+from typing import Dict, List, Optional, Union
 import sys
 
 from PIL import Image
 
-import boto3
+import aioboto3 # type: ignore
 
 from botocore.exceptions import ClientError
 from pydantic import BaseModel
@@ -58,85 +59,6 @@ def default_context() -> Dict[str, Union[str,bool]]:
         "baseurl" : meme_config_load().baseurl,
     }
 
-# pylint: disable=too-few-public-methods
-class MemeBucket:
-    """ handles s3 things """
-    def __init__(self, meme_config: MemeConfig):
-        """ init """
-        self.session = boto3.Session(
-            aws_access_key_id=meme_config.aws_access_key_id,
-            aws_secret_access_key=meme_config.aws_secret_access_key,
-            region_name=meme_config.aws_region,
-        )
-        if meme_config.endpoint_url is not None:
-
-            self.s3_resource = self.session.resource(
-                's3',
-                endpoint_url=meme_config.endpoint_url,
-                config=boto3.session.Config(signature_version='s3v4'), # type: ignore
-                )
-        else:
-            self.s3_resource = self.session.resource(
-                's3',
-                )
-        self.bucket = self.s3_resource.Bucket(meme_config.bucket)
-
-    @property
-    def objects(self) -> Generator[Any,None,None]:
-        """ returns the objects iterator """
-        iterator: Generator[Any,None,None] = self.bucket.objects.iterator()
-        return iterator
-
-# pylint: disable=too-few-public-methods
-class MemeImage:
-    """ gets an object """
-    def __init__(self, meme_config: MemeConfig):
-        """ init """
-        self.bucket = meme_config.bucket
-        self.session = boto3.Session(
-            aws_access_key_id=meme_config.aws_access_key_id,
-            aws_secret_access_key=meme_config.aws_secret_access_key,
-            region_name=meme_config.aws_region,
-        )
-        if meme_config.endpoint_url is not None:
-            self.s3_resource = self.session.resource(
-                's3',
-                endpoint_url=meme_config.endpoint_url,
-                config=boto3.session.Config(signature_version='s3v4'), # type: ignore
-                )
-        else:
-            self.s3_resource = self.session.resource(
-                's3',
-                )
-
-    def get(self, filename: str) -> Optional[bytes]:
-        """ gets an image"""
-        try:
-            image = self.s3_resource.Object(self.bucket, filename)
-            if "Body" in image.get():
-                body: bytes = image.get()["Body"]
-                return body
-        except ClientError as error_message:
-            print(f"ClientError pulling '{filename}': {error_message}", file=sys.stderr)
-        return None
-
-    def get_as_http(self, filename: str) -> Union[HTMLResponse,StreamingResponse]:
-        """ returns a Response object """
-        try:
-            image = self.s3_resource.Object(self.bucket, filename)
-            if "Body" in image.get():
-                body: bytes = image.get()["Body"]
-                return StreamingResponse(body)
-        except ClientError as error_message:
-            error_text = f"ClientError pulling '{filename}': {error_message}"
-            print(error_text, file=sys.stderr)
-            response_status = 500
-            if "ResponseMetadata" in error_message.response:
-                if "HTTPStatusCode" in error_message.response["ResponseMetadata"]:
-                    response_status = error_message.response["ResponseMetadata"]["HTTPStatusCode"]
-            return HTMLResponse(error_text,status_code=response_status )
-
-
 CSS_BASEDIR = Path(f"{os.path.dirname(__file__)}/css/").resolve().as_posix()
 IMAGES_BASEDIR = Path(f"{os.path.dirname(__file__)}/images/").resolve().as_posix()
 JS_BASEDIR = Path(f"{os.path.dirname(__file__)}/js/").resolve().as_posix()
@@ -149,26 +71,62 @@ app.add_middleware(GZipMiddleware, minimum_size=1000)
 @app.get("/allimages")
 async def all_images() -> Dict[str, List[str]]:
     """ returns all the images """
-    bucket = MemeBucket(meme_config_load())
-    return { "images" : [ image.key for image in bucket.objects ]}
+    meme_config = meme_config_load()
+    session = aioboto3.Session(
+        aws_access_key_id=meme_config.aws_access_key_id,
+        aws_secret_access_key=meme_config.aws_secret_access_key,
+        region_name=meme_config.aws_region,
+    )
+
+    if meme_config.endpoint_url is not None:
+        async with session.resource('s3', endpoint_url=meme_config.endpoint_url) as s3_resource:
+            bucket = await s3_resource.Bucket(meme_config.bucket)
+            results = { "images" : [ image.key async for image in bucket.objects.iterator() ]}
+    else:
+        async with session.resource('s3') as s3_resource:
+            bucket = await s3_resource.Bucket(meme_config.bucket)
+            results = { "images" : [ image.key async for image in bucket.objects.iterator() ]}
+
+    print(results, file=sys.stderr)
+    return results
+
+def get_aioboto3_session() -> aioboto3.Session:
+    """ gets a session """
+
+    meme_config = meme_config_load()
+    return aioboto3.Session(
+        aws_access_key_id=meme_config.aws_access_key_id,
+        aws_secret_access_key=meme_config.aws_secret_access_key,
+        region_name=meme_config.aws_region,
+    )
 
 @app.get("/thumbnail/{filename}")
 async def get_thumbnail(filename: str) -> Union[HTMLResponse,StreamingResponse]:
     """ returns an image thumbnailed """
-
-    image_class = MemeImage(meme_config_load())
-    image = image_class.get(filename)
-
-    if image is None:
-        return HTMLResponse(content="", status_code=404)
-
-    bgcolour = (255,255,255)
+    async with get_aioboto3_session().client(
+        's3',
+        endpoint_url=meme_config_load().endpoint_url,
+        ) as s3_client:
+        try:
+            image_object = await s3_client.get_object(Bucket=meme_config_load().bucket, Key=filename)
+            if "Body" in image_object:
+                content = await image_object['Body'].read()
+            else:
+                return HTMLResponse(status_code=404)
+        except ClientError as error_message:
+            error_text = f"ClientError pulling '{filename}': {error_message}"
+            print(error_text, file=sys.stderr)
+            response_status = 500
+            if "ResponseMetadata" in error_message.response:
+                if "HTTPStatusCode" in error_message.response["ResponseMetadata"]:
+                    response_status = error_message.response["ResponseMetadata"]["HTTPStatusCode"]
+            return HTMLResponse(error_text,status_code=response_status )
     tmpstorage = BytesIO()
-    with Image.open(image) as tempimage:
+    with Image.open(BytesIO(content)) as tempimage:
         tempimage.thumbnail(THUMBNAIL_DIMENSIONS)
         # tempimage = tempimage.resize(THUMBNAIL_DIMENSIONS).convert('RGB')
         tempimage = tempimage.convert('RGB')
-        expanded = Image.new('RGB', THUMBNAIL_DIMENSIONS, bgcolour)
+        expanded = Image.new('RGB', THUMBNAIL_DIMENSIONS, (255,255,255))
 
         paste_x = 0
         paste_y = 0
@@ -214,8 +172,31 @@ async def get_image_info(filename: str) -> HTMLResponse:
 @app.get("/image/{filename}")
 async def get_image(filename: str) -> Union[HTMLResponse,StreamingResponse]:
     """ returns an image """
-    image_class = MemeImage(meme_config_load())
-    return image_class.get_as_http(filename)
+    meme_config = meme_config_load()
+    session = get_aioboto3_session()
+
+    async with session.client('s3', endpoint_url=meme_config.endpoint_url) as s3_client:
+        try:
+            image_object = await s3_client.get_object(Bucket=meme_config.bucket, Key=filename)
+            ob_info = image_object["ResponseMetadata"]["HTTPHeaders"]
+            if "Body" in image_object:
+                content = await image_object['Body'].read()
+            else:
+                print("Couldn't find body!", file=sys.stderr)
+                return HTMLResponse(status_code=404)
+        except ClientError as error_message:
+            error_text = f"ClientError pulling '{filename}': {error_message}"
+            print(error_text, file=sys.stderr)
+            response_status = 500
+            if "ResponseMetadata" in error_message.response:
+                if "HTTPStatusCode" in error_message.response["ResponseMetadata"]:
+                    response_status = error_message.response["ResponseMetadata"]["HTTPStatusCode"]
+            return HTMLResponse(error_text,status_code=response_status )
+    headers = {
+        "content_type" : ob_info["content-type"],
+        "content_length" : ob_info["content-length"],
+    }
+    return StreamingResponse(BytesIO(content), headers=headers)
 
 @app.get("/static/js/{filename}")
 async def jsfile(filename: str) -> Union[FileResponse, HTMLResponse]:
