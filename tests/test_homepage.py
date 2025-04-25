84ae9f9fad0c6d9ef08b403b111691fbdd44510c
James Hodgkinson <james@terminaloutcomes.com>
2025-04-25 11:14:26 +1000

    updating coverage

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,4 +1,4 @@
-""" tests the basics of the home page """
+"""tests the basics of the home page"""
 
 from fastapi.testclient import TestClient
 
@@ -6,9 +6,16 @@ from memes_api import app
 
 client = TestClient(app)
 
+
 def test_homepage() -> None:
-    """ grabs the homepage """
+    """grabs the homepage"""
     for _ in range(100):
         response = client.get("/")
         assert response.status_code == 200
         assert "Memes!" in response.content.decode("utf-8")
+
+
+def test_get_allimages() -> None:
+    """grabs all images"""
+    response = client.get("/allimages")
+    assert response.status_code == 200
